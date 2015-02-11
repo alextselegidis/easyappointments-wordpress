7aa9d8ee127913dcdad175ddfd6158d1d7afde47
Alex Tselegidis <alextselegidis@gmail.com>
2015-02-12 00:19:37 +0100

    Added sample test file

---
+++ test/PluginTest.php
@@ -0,0 +1,17 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - WordPress Plugin 
+ *
+ * @license GPL2+
+ * @copyright A.Tselegidis (C) 2015
+ * @link http://easyappointments.org
+ * @since v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+require_once __DIR__ . '/../src/core/Plugin.php';
+
+class PluginTest extends PHPUnit_Framework_TestCase {   
+    public function testSample() {
+        $this->assertTrue(true);
+    }
+}
