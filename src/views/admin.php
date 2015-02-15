7d2e5b9c5768551fd45961274d6f8f58b80025ef
Alex Tselegidis <alextselegidis@gmail.com>
2015-02-15 21:09:07 +0100

    Added view files for wp-admin section

---
+++ src/views/admin.php
@@ -0,0 +1,15 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - WordPress Plugin 
+ *
+ * @license GPL2+
+ * @copyright A.Tselegidis (C) 2015
+ * @link http://easyappointments.org
+ * @since v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/* 
+ * WordPress administration page for the plugin will accept user settings and display
+ * information about the plugin and the supported operations. 
+ */
+
