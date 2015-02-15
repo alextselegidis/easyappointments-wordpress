4b6ef38f67475c266b658eaa81b0e7251aaaf56c
Alex Tselegidis <alextselegidis@gmail.com>
2015-02-15 21:23:55 +0100

    Added forgotten test files.

---
+++ test/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - WordPress Plugin 
+ *
+ * @license GPL2+
+ * @copyright A.Tselegidis (C) 2015
+ * @link http://easyappointments.org
+ * @since v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+// @todo Define autoloader for the plugin classes so that the PHPUnit mock builder
+// automatically finds the original classes that need to be mocked.
+
+// Load required libraries and classes.
+require_once __DIR__ . '/mocks/WpFunctions.php';
---
+++ test/mocks/WpFunctions.php
@@ -0,0 +1,21 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - WordPress Plugin 
+ *
+ * @license GPL2+
+ * @copyright A.Tselegidis (C) 2015
+ * @link http://easyappointments.org
+ * @since v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * WpFunctions Class
+ *  
+ * This class will register global functions that will return assigned results used 
+ * by the tests in order to mock the WP global functions ecosystem. 
+ */
+class WpFunctions {
+    public static function register($name, $returnValue) {
+        
+    }
+} 
