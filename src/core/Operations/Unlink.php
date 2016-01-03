2bb4f182656f2766fa7760697e5ef59dc8d326be
Alex Tselegidis <alextselegidis@gmail.com>
2016-01-03 11:39:11 +0100

    Added new operation files.

---
+++ src/core/Operations/Unlink.php
@@ -0,0 +1,63 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - WordPress Plugin
+ *
+ * @license GPLv3
+ * @copyright A.Tselegidis (C) 2015
+ * @link http://easyappointments.org
+ * @since v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+namespace EAWP\Operations;
+
+use EAWP\Core\Plugin;
+use EAWP\Core\ValueObjects\Path;
+
+/**
+ * Unlink Operation
+ *
+ * This class implements the "unlink" operation of a given connection between WordPress
+ * and Easy!Appointments. It is also possible to entirely remove the installation files
+ * and database tables.
+ *
+ * @todo Implement Operation
+ */
+class Unlink implements OperationInterface {
+    /**
+     * Plugin Instance
+     *
+     * @var \EAWP\Core\Plugin
+     */
+    protected $plugin;
+
+    /**
+     * Easy!Appointments Installation Path
+     *
+     * @var \EAWP\Core\ValueObjects\Path
+     */
+    protected $path;
+
+    /**
+     * Class Constructor
+     *
+     * @param Plugin $plugin The plugin instance.
+     * @param Path $path Contains the Easy!Appointments installation path.
+     * @param bool $removeFilesAndDatabase (optional) Whether to also remove the E!A files and database tables.
+     */
+    public function __construct(Plugin $plugin, Path $path, $removeFilesAndDatabase = false) {
+        if (!is_bool($removeFilesAndDatabase)) {
+            throw new \InvalidArgumentException('Invalid argument provided (expected bool got "'
+                    . gettype($removeFilesAndDatabase) . '"): ' . $removeFilesAndDatabase);
+        }
+    }
+
+    /**
+     * Invoke the unlink operation.
+     *
+     * This method will reset the WordPress options which will automatically disable any
+     * used shortcodes.
+     */
+    public funtion invoke() {
+
+    }
+}
---
+++ src/core/Operations/VerifyState.php
@@ -0,0 +1,68 @@
+<?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - WordPress Plugin
+ *
+ * @license GPLv3
+ * @copyright A.Tselegidis (C) 2015
+ * @link http://easyappointments.org
+ * @since v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+namespace EAWP\Operations;
+
+use EAWP\Core\Plugin;
+use EAWP\Core\ValueObjects\Path;
+
+/**
+ * Verify State Operation
+ *
+ * This class implements the "verify state" operation of the current connection. This operation
+ * will check if Easy!Appointments is correctly connected to WordPress.
+ *
+ * @todo Implement Operation
+ */
+class VerifyState implements OperationInterface {
+    /**
+     * Instance of Easy!Appointments WP Plugin
+     *
+     * @var EAWP\Core\Plugin
+     */
+    protected $plugin;
+
+    /**
+     * Easy!Appointments Installation Path
+     *
+     * @var EAWP\Core\ValueObjects\Path
+     */
+    protected $path;
+
+    /**
+     * Easy!Appointments Installation URL ($base_url)
+     *
+     * @var EAWP\Core\ValueObjects\Url
+     */
+    protected $url;
+
+    /**
+     * Class Constructor
+     *
+     * @param EAWP\Core\Plugin $plugin Easy!Appointments WordPress Plugin Instance
+     * @param EAWP\Core\ValueObjects\Path $path Easy!Appointments installation path (provided from user).
+     * @param EAWP\Core\ValueObjects\Url $url Easy!Appointments installation url (provided from user).
+     */
+    public function __construct(Plugin $plugin, Path $path, Url $url) {
+        $this->plugin = $plugin;
+        $this->path = $path;
+        $this->url = $url;
+    }
+
+    /**
+     * Invode the unlink operation.
+     *
+     * This method will reset the WordPress options which will automatically disable any
+     * used shortcodes.
+     */
+    public funtion invoke() {
+
+    }
+}
