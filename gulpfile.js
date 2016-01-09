34220fffd4793642f21a6d4065df02610b8465a6
Alex Tselegidis <alextselegidis@gmail.com>
2016-01-09 23:27:03 +0100

    Implemented the 'gulp doc' task. It will generate ApiGen documentation for the plugin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
-/*
- * Implement the following tasks:
- *
- * - configure
- * - build
- * - doc
- * - test
- *
+var gulp = require('gulp'),
+    exec = require('child_process').execSync,
+    fs = require('fs-extra');
+
+/**
+ * Generate Code Documentation (ApiGen)
  */
+gulp.task('doc', function(done) {
+    fs.removeSync('doc/apigen');
+    var command = 'php doc/apigen.phar generate -s "src" -d "doc/apigen" --exclude "*ea-vendor*" '
+            + '--todo --template-theme "bootstrap"'
+    exec(command, function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+    });
+
+    done();
+});
