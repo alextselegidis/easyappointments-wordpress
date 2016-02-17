6e68270962cd93134de37fe622916e8a409a64f1
Alex Tselegidis <alextselegidis@gmail.com>
2016-02-17 21:49:03 +0100

    Corrected the indentation of iframe.js file.

--- src/assets/js/iframe.js
+++ src/assets/js/iframe.js
@@ -1,42 +1,42 @@
 /* ----------------------------------------------------------------------------
- * Easy!Appointments - WordPress Plugin
- *
- * @license GPLv3
- * @copyright A.Tselegidis (C) 2016
- * @link http://easyappointments.org
- * @since v1.0.0
- * ---------------------------------------------------------------------------- */
+* Easy!Appointments - WordPress Plugin
+*
+* @license GPLv3
+* @copyright A.Tselegidis (C) 2016
+* @link http://easyappointments.org
+* @since v1.0.0
+* ---------------------------------------------------------------------------- */
 
 /**
- * Easy!Appointments IFrame Handler
- *
- * This module will handle the booking wizard iframe operations.
- */
- jQuery(function($) {
+* Easy!Appointments IFrame Handler
+*
+* This module will handle the booking wizard iframe operations.
+*/
+jQuery(function($) {
 
-     'use strict';
+    'use strict';
 
-     var $iframe = $('.easyappointments-wp-iframe'),
-        initialHeight = $iframe.height();
+    var $iframe = $('.easyappointments-wp-iframe'),
+    initialHeight = $iframe.height();
 
-     // Add "resize" watcher on the iframe content element so that the iframe always show the
-     // whole page content (mostly useful for mobile views - vertical resize).
-     function _resizeWatcher() {
-         var $iframeContent = $('#book-appointment-wizard', $iframe.contents()),
-             lastHeight = $iframeContent.height();
+    // Add "resize" watcher on the iframe content element so that the iframe always show the
+    // whole page content (mostly useful for mobile views - vertical resize).
+    function _resizeWatcher() {
+        var $iframeContent = $('#book-appointment-wizard', $iframe.contents()),
+            lastHeight = $iframeContent.height();
 
-         setInterval(function() {
-             $iframeContent = $('#book-appointment-wizard', $iframe.contents());
+        setInterval(function() {
+            $iframeContent = $('#book-appointment-wizard', $iframe.contents());
 
-             if (lastHeight === $iframeContent.height()) {
-                 return;
-             }
+            if (lastHeight === $iframeContent.height()) {
+                return;
+            }
 
-             var newHeight = ($iframeContent.height() > initialHeight) ? $iframeContent.height() : initialHeight;
-             $iframe.height(newHeight);
-             lastHeight = $iframeContent.height();
-         }, 500);
-     }
+            var newHeight = ($iframeContent.height() > initialHeight) ? $iframeContent.height() : initialHeight;
+            $iframe.height(newHeight);
+            lastHeight = $iframeContent.height();
+        }, 500);
+    }
 
-     $iframe.on('load', _resizeWatcher);
+    $iframe.on('load', _resizeWatcher);
 });
