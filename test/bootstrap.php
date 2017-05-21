0b9ec363084445e19779e5acd02aa25d82fc162d
alext <alextselegidis@gmail.com>
2017-05-21 15:23:41 +0200

    Updated bootstrap.php

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -8,6 +8,9 @@
  * @since v1.0.0
  * ---------------------------------------------------------------------------- */
 
+declare(strict_types = 1);
+error_reporting(E_ALL);
+
 define('EAWP_BASEPATH', __DIR__ . '/../src');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'db_name');
@@ -15,12 +18,4 @@ define('DB_USER', 'db_user');
 define('DB_PASSWORD', 'db_password');
 
 // Load required libraries and classes for the tests.
-require_once __DIR__ . '/includes/Filesystem.php';
-require_once __DIR__ . '/mocks/WPDB.php';
-require_once __DIR__ . '/mocks/WpMock.php';
-require_once __DIR__ . '/mocks/WpFunctions.php';
 require_once EAWP_BASEPATH . '/core/Autoload.php';
-
-$loader = new EAWP\Core\Autoload;
-$loader->register();
-$loader->addNamespace('EAWP\Core', EAWP_BASEPATH . '/core');
