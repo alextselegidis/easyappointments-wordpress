fe90b8f99d35c63b67f56753e5f0f7c7e72daecc
alext <alextselegidis@gmail.com>
2017-06-15 10:14:38 +0200

    Correction in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -37,8 +37,8 @@ gulp.task('build', ['styles', 'scripts'], function(done) {
  */
 gulp.task('doc', function(done) {
     fs.removeSync('doc/apigen');
-    const command = 'php doc/apigen.phar generate -s "src" -d "doc/apigen" --exclude "*ea-vendor*" '
-            + '--todo --template-theme "bootstrap"'
+    const command = 'php doc/apigen.phar generate -s "src" -d "doc/apigen" --exclude "*vendor*" '
+            + '--todo --template-theme "bootstrap"';
     exec(command, function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
