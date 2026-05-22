102a8be2e682700aa44991569656c74b70c20665
Alex Tselegidis <alextselegidis@gmail.com>
2026-05-22 15:07:25 +0200

    Added a new block for Easy!Appointments.

---
+++ blocks/easyappointments/index.asset.php
@@ -0,0 +1,10 @@
+<?php return array(
+    'dependencies' => array(
+        'wp-blocks',
+        'wp-block-editor',
+        'wp-components',
+        'wp-element',
+        'wp-i18n',
+    ),
+    'version' => '1.0.0',
+);
